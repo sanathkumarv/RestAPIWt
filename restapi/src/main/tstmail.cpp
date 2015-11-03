31a96842554b952029bb7bc3b5f53de901284739
Sanath Kumar <varamballysanathkumar@gmail.com>
2015-11-03 16:24:05 +0530

    google webmaster verification

--- restapi/src/main/tstmail.cpp
+++ restapi/src/main/tstmail.cpp
@@ -10,21 +10,23 @@ int main()
 		CSmtp mail;
 		mail.SetSMTPServer("smtp.gmail.com",465);
     		mail.SetSecurityType(USE_SSL);
+		mail.m_bHTML =true;
+
 
 	//	mail.SetSMTPServer("smtp.gmail.com",25);
 		mail.SetLogin("varamballysanathkumar@gmail.com");
 		mail.SetPassword("Gayathri123");
   		mail.SetSenderName("Sanath");
-  		mail.SetSenderMail("varamballysanathkumar@domain.com");
+  		mail.SetSenderMail("varamballysanathkumar@gmail.com");
   		mail.SetReplyTo("varamballysanathkumar@gmail.com");
   		mail.SetSubject("The message");
   		mail.AddRecipient("sanathkumar.v@cspl.com");
   		mail.SetXPriority(XPRIORITY_NORMAL);
   		mail.SetXMailer("The Bat! (v3.02) Professional");
-  		mail.AddMsgLine("Hello,");
+  		mail.AddMsgLine("<b>Hello,");
 		mail.AddMsgLine("");
 		mail.AddMsgLine("...");
-		mail.AddMsgLine("How are you today?");
+		mail.AddMsgLine("How are you today?</b>");
 		mail.AddMsgLine("");
 		mail.AddMsgLine("Regards");
 		mail.ModMsgLine(5,"regards");
